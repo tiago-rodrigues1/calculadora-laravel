6a8d6262215e4854f8363705740e4fe664230465
tiago-rodrigues1 <tiagords127@gmail.com>
2022-11-17 11:53:20 -0300

    envia dados dinamicamente para a view

--- app/Http/Controllers/OperacaoController.php
+++ app/Http/Controllers/OperacaoController.php
@@ -6,18 +6,30 @@ use Illuminate\Http\Request;
 
 class OperacaoController extends Controller {
     public function somar($n1, $n2) {
-        return $n1 + $n2;
+        $resultado = $n1 + $n2;
+        $sinal = '+';
+
+        return view('resultado', compact('resultado', 'n1', 'n2', 'sinal'));
     }
 
     public function subtrair($n1, $n2) {
-        return $n1 - $n2;
+        $resultado = $n1 - $n2;
+        $sinal = '-';
+
+        return view('resultado', compact('resultado', 'n1', 'n2', 'sinal'));
     }
 
     public function multiplicar($n1, $n2) {
-        return $n1 * $n2;
+        $resultado = $n1 * $n2;
+        $sinal = 'x';
+
+        return view('resultado', compact('resultado', 'n1', 'n2', 'sinal'));
     }
 
     public function dividir($n1, $n2) {
-        return $n1 / $n2;
+        $resultado = $n1 / $n2;
+        $sinal = '/';
+
+        return view('resultado', compact('resultado', 'n1', 'n2', 'sinal'));
     }
 }
